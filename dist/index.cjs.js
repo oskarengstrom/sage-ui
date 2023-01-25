c4f107b3b051cdd9c78d7e9390b96c949da50884
oskarengstrom <hi@oskarengstrom.com>
2023-01-25 09:24:56 +0100

    fixed color bug 2

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -61,17 +61,24 @@ var responsiveProp = function responsiveProp(_ref) {
 //   return s.color == strColor;
 // };
 
-var isValidColor = function isValidColor(strColor) {
-  if (typeof window === "undefined") {
-    // On the server-side, use a regular expression to check if the string matches a valid color format
-    return /^(#[0-9a-f]{3}|#[0-9a-f]{6}|(rgb|hsl)a?\([^\)]*\))$/i.test(strColor);
-  } else {
-    // On the client-side, use the Option.style object to check if the string is a valid color
-    var s = new Option().style;
-    s.color = strColor;
-    return s.color == strColor;
-  }
-};
+// export const isValidColor = (strColor) => {
+//   if (typeof window === "undefined") {
+//     // On the server-side, use a regular expression to check if the string matches a valid color format
+//     return /^(#[0-9a-f]{3}|#[0-9a-f]{6}|(rgb|hsl)a?\([^\)]*\))$/i.test(
+//       strColor
+//     );
+//   } else {
+//     // On the client-side, use the Option.style object to check if the string is a valid color
+//     var s = new Option().style;
+//     s.color = strColor;
+//     return s.color == strColor;
+//   }
+// };
+
+function isValidColor(strColor) {
+  // Use a regular expression to check if the string matches a valid color format
+  return /^(#[0-9a-f]{3}|#[0-9a-f]{6}|(rgb|hsl)a?\([^\)]*\))$/i.test(strColor);
+}
 
 var findValueInObject = function findValueInObject(str, obj) {
   var keys = str.split(".");
--- src/utils/isValidColor.js
+++ src/utils/isValidColor.js
@@ -5,16 +5,21 @@
 //   return s.color == strColor;
 // };
 
-export const isValidColor = (strColor) => {
-  if (typeof window === "undefined") {
-    // On the server-side, use a regular expression to check if the string matches a valid color format
-    return /^(#[0-9a-f]{3}|#[0-9a-f]{6}|(rgb|hsl)a?\([^\)]*\))$/i.test(
-      strColor
-    );
-  } else {
-    // On the client-side, use the Option.style object to check if the string is a valid color
-    var s = new Option().style;
-    s.color = strColor;
-    return s.color == strColor;
-  }
-};
+// export const isValidColor = (strColor) => {
+//   if (typeof window === "undefined") {
+//     // On the server-side, use a regular expression to check if the string matches a valid color format
+//     return /^(#[0-9a-f]{3}|#[0-9a-f]{6}|(rgb|hsl)a?\([^\)]*\))$/i.test(
+//       strColor
+//     );
+//   } else {
+//     // On the client-side, use the Option.style object to check if the string is a valid color
+//     var s = new Option().style;
+//     s.color = strColor;
+//     return s.color == strColor;
+//   }
+// };
+
+export function isValidColor(strColor) {
+  // Use a regular expression to check if the string matches a valid color format
+  return /^(#[0-9a-f]{3}|#[0-9a-f]{6}|(rgb|hsl)a?\([^\)]*\))$/i.test(strColor);
+}
